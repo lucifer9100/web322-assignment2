9901e9c139e84e0834523b1dac2999b0d8d1da43
John Bista <jbista2@myseneca.ca>
2025-02-02 13:10:25 -0500

    second commit

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/********************************************************************************
+*  WEB322 – Assignment 02
+* 
+*  I declare that this assignment is my own work in accordance with Seneca's
+*  Academic Integrity Policy:
+* 
+*  https://www.senecapolytechnic.ca/about/policies/academic-integrity-policy.html
+* 
+*  Name: John Bista Student ID: 153185236 Date: 2025/02/2025
+*
+********************************************************************************/
+
 const express = require('express');
 const projectData = require('./modules/projects');
 
